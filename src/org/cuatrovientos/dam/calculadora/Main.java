cff8c8f80d0ab7771348101d754dc2c6254c5078
Jarol Rojas Reyes <jr.rojas0327@gmail.com>
2025-01-14 23:06:38 +0100

    Arreglar error al dividir por 0

--- src/org/cuatrovientos/dam/calculadora/Main.java
+++ src/org/cuatrovientos/dam/calculadora/Main.java
@@ -1,4 +1,4 @@
- package org.cuatrovientos.dam.calculadora;
+package org.cuatrovientos.dam.calculadora;
 
 import java.util.Scanner;
 
@@ -8,32 +8,33 @@ public class Main {
 		// Definición de variables necesarias
 		Scanner scan = new Scanner(System.in);
 		int opcion;
+
 		// Mostrar menú y elegir opción
 		do {
-			System.out.println("\tCALCULADORA\n" + 
-					"============================\n" +
-					"Elige una opción\n" +
-					"1. Suma\n" +
-					"2. Resta\n" +
-					"3. Multiplicación\n" +
-					"4. División\n" +
-					"5. Salir del programa\n"+
-					"============================");
+			mostrarMenu();
 			opcion = leerOpcion(scan);
+
 			// Realizar operación de la opción selecionada
 			switch (opcion) {
-				case 1 -> realizarOperaciones(scan, "SUMA", (a, b) -> a + b);
-				case 2 -> realizarOperaciones(scan, "RESTA", (a, b) -> a - b);
-				case 3 -> realizarOperaciones(scan, "MULTIPLICACIÓN", (a, b) -> a * b);
-				case 4 -> realizarOperaciones(scan, "DIVISIÓN", (a, b) -> a / b);
+				case 1 -> realizarOperacion(scan, "SUMA", (a, b) -> a + b);
+				case 2 -> realizarOperacion(scan, "RESTA", (a, b) -> a - b);
+				case 3 -> realizarOperacion(scan, "MULTIPLICACIÓN", (a, b) -> a * b); //TODO ARREGLAR LIMITE DE DOUBLE
+				case 4 -> realizarOperacion(scan, "DIVISIÓN", (a, b) -> a / b);
 				case 5 -> System.out.println("Programa finalizado.");
 			}
 		} while (opcion != 5);
-		
+
 		// Cerrar scanner
 		scan.close();
 	}
-	//	Leer opción y validar
+
+	private static void mostrarMenu() {
+		System.out.println("\tCALCULADORA\n" + "============================\n" + "Elige una opción\n" + "1. Suma\n"
+				+ "2. Resta\n" + "3. Multiplicación\n" + "4. División\n" + "5. Salir del programa\n"
+				+ "============================");
+	}
+
+	// Leer opción y validar
 	private static int leerOpcion(Scanner scan) {
 		int opcion;
 		do {
@@ -50,17 +51,26 @@ public class Main {
 		} while (opcion > 5 || opcion < 1);
 		return opcion;
 	}
-	//	Realizar operaciones según la opcion elegida
-	private static void realizarOperaciones(Scanner scan, String operacion, Operacion op) {
+
+	// Realizar operaciones según la opcion elegida
+	private static void realizarOperacion(Scanner scan, String operacion, Operacion op) {
 		System.out.println("\t" + operacion + "\n");
 		double num1 = leerNumero(scan, "Ingresa el primer número: ");
-		double num2 = leerNumero(scan, "Ingresa el segundo número: ");
-					
+		double num2;
+		if (operacion.equals("DIVISIÓN")) {
+			do {
+				num2 = leerNumero(scan, "Ingresa el segundo número (no puede ser 0): ");
+				if (num2 == 0) {
+					System.out.println("El divisor no puede ser 0. Intenta nuevamente.");
+				}
+			} while (num2 == 0);
+		}else {
+			num2 = leerNumero(scan, "Ingresa el segundo número: ");
+
 		System.out.println("\nEl resultado de la " + operacion + " es: " + op.calcular(num1, num2) + "\n\n");
-//		FIXME (Error, al dividir por 0 se rompe el programa)
 	}
 
-	//	Validar entrada de números
+	// Validar entrada de números
 	private static double leerNumero(Scanner scan, String mensaje) {
 		System.out.print(mensaje);
 		while (!scan.hasNextDouble()) {
@@ -70,6 +80,7 @@ public class Main {
 		}
 		return scan.nextDouble();
 	}
+
 	interface Operacion {
 		double calcular(double a, double b);
 	}
